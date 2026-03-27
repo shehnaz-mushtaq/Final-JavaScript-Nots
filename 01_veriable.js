adae3dd8a6297f201e49771ba4e23e9f075c3314
Shehnaz Mushtaq <mushtaqali22g@gmail.com>
2026-03-26 18:44:11 -0700

    Add files via upload

---
+++ 01_veriable.js
@@ -0,0 +1,11 @@
+const userName ="Nasreen"
+let userEmail = "Nasreen@gmail.com"
+var userPassword = 12345
+
+userCity = "Usman Wala"
+
+// userRegion; https://github.com/username/JavaProject.git
+
+console.table([userName, userEmail,userPassword,  userCity]);
+
+console.log("Finally it's done");
