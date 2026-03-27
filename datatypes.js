1873f4fac64d563d989804faf47b2069be22ffbb
Shehnaz Mushtaq <mushtaqali22g@gmail.com>
2026-03-27 04:07:50 -0700

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,71 @@
+"use strict" ; // Treat all js code as a newer version
+
+//Tc39.es  for echma statanders
+// mdm for documentations
+
+
+// DATATYpes
+
+
+// ## 🔹 Primitive Data Types
+
+// **1. String**
+// 👉 Text ko store karta hai
+// **Example:** `"Ali"`
+
+// **2. Number**
+// 👉 Numbers ko store karta hai
+// **Example:** `10`, `3.5`
+
+// **3. Boolean**
+// 👉 True ya False value hoti hai
+// **Example:** `true`
+
+// **4. Undefined**
+// 👉 Variable declare ho lekin value na ho
+// **Example:**
+
+// ```js
+// let x;
+// ```
+
+// **5. Null**
+// 👉 Empty / intentionally no value
+// **Example:**
+
+// ```js
+// let y = null;
+// ```
+
+// **6. BigInt**
+// 👉 Bohat bara number store karta hai
+// **Example:** `123456789n`
+
+// **7. Symbol**
+// 👉 Unique value create karta hai
+// **Example:**
+
+// ```js
+// let id = Symbol("id");
+// ```
+
+// ---
+
+// ## 🔹 Non-Primitive
+
+// **8. Object**
+// 👉 Multiple values ko ek jagah store karta hai
+// **Example:**
+
+// ```js
+// let person = {name: "Ali", age: 20};
+
+//One Line Trick:
+
+// 👉 **“SSNNBBU + Object”**
+// (String, Number, Boolean, Undefined, Null, BigInt, Symbol + Object
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(String));
+console.log(typeof(Number));
