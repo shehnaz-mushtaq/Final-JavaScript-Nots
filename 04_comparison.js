6729c0564d4099b20efd18dc3b4d29faa2101fbe
Shehnaz Mushtaq <mushtaqali22g@gmail.com>
2026-03-28 05:11:28 -0700

    Add files via upload

---
+++ 04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  (Strict comparison   Mean k sirf data ko check ni krta blky data type ko b check krta hai)
+
+console.log("2" === 2);
